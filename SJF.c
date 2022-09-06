0e0e53296352f1b5318de4d078390befdbfbcfc1
214G5A0503 <112072414+214G5A0503@users.noreply.github.com>
2022-09-06 12:30:41 +0530

    Anitha Created SJF.c

---
+++ SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.Anitha
+   Roll No: 214G5A0503
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
